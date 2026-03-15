de16b711ef5205e370e80281db86fc7a3942c6df
Marianne Feng <feng.marianne@gmail.com>
2026-03-15 13:10:21 -0400

    add kasa lib + special actions for slider + pads

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go-home
 
 go 1.24.2
 
-require gitlab.com/gomidi/midi/v2 v2.3.23 // indirect
+require (
+	github.com/cloudkucooland/go-kasa v0.0.0-20251225171943-cfa233a69f1e
+	gitlab.com/gomidi/midi/v2 v2.3.23
+)
--- main.go
+++ main.go
@@ -5,12 +5,91 @@ import (
 	"os"
 	"os/signal"
 
+	kasa "github.com/cloudkucooland/go-kasa"
 	"gitlab.com/gomidi/midi/v2"
 	_ "gitlab.com/gomidi/midi/v2/drivers/rtmididrv"
 )
 
+const (
+	padChannel       = 9
+	padFlowerLamp    = 36
+	padMushroomLamp  = 37
+	knobChannel      = 0
+	knobMushroomCC   = 1
+	pitchBendChannel = 0
+)
+
+func discoverKasa() {
+	fmt.Println("Discovering Kasa devices...")
+	devices, err := kasa.BroadcastDiscovery(3, 1)
+	if err != nil {
+		fmt.Printf("Kasa discovery error: %s\n", err)
+		return
+	}
+	fmt.Printf("Found %d Kasa device(s):\n", len(devices))
+	for ip, info := range devices {
+		fmt.Printf("  %s: %s [%s] relay=%d brightness=%d%%\n",
+			ip, info.Alias, info.Model, info.RelayState, info.Brightness)
+	}
+	fmt.Println()
+}
+
+func handleMIDI(msg midi.Message, timestampms int32) {
+	var ch, key, vel, cc, val uint8
+	var pitchRel int16
+	var bt []byte
+
+	switch {
+	case msg.GetNoteStart(&ch, &key, &vel):
+		if ch == padChannel {
+			switch key {
+			case padFlowerLamp:
+				fmt.Printf("[%6dms] TOGGLE flower lamp (vel=%d)\n", timestampms, vel)
+				return
+			case padMushroomLamp:
+				fmt.Printf("[%6dms] TOGGLE mushroom lamp (vel=%d)\n", timestampms, vel)
+				return
+			}
+		}
+		fmt.Printf("[%6dms] NoteOn    ch=%d  key=%3d  vel=%3d  (%s)\n",
+			timestampms, ch, key, vel, midi.Note(key))
+
+	case msg.GetNoteEnd(&ch, &key):
+		if ch == padChannel && (key == padFlowerLamp || key == padMushroomLamp) {
+			return
+		}
+		fmt.Printf("[%6dms] NoteOff   ch=%d  key=%3d           (%s)\n",
+			timestampms, ch, key, midi.Note(key))
+
+	case msg.GetPitchBend(&ch, &pitchRel, nil):
+		if ch == pitchBendChannel {
+			fmt.Printf("[%6dms] BRIGHTNESS flower lamp  pitch=%d\n", timestampms, pitchRel)
+			return
+		}
+		fmt.Printf("[%6dms] PitchBend ch=%d  pitch=%d\n", timestampms, ch, pitchRel)
+
+	case msg.GetControlChange(&ch, &cc, &val):
+		if ch == knobChannel && cc == knobMushroomCC {
+			fmt.Printf("[%6dms] BRIGHTNESS mushroom lamp  val=%d\n", timestampms, val)
+			return
+		}
+		fmt.Printf("[%6dms] CC        ch=%d  cc=%3d   val=%3d\n", timestampms, ch, cc, val)
+
+	case msg.GetAfterTouch(&ch, &val):
+		fmt.Printf("[%6dms] AfterTch  ch=%d  val=%3d\n", timestampms, ch, val)
+
+	case msg.GetSysEx(&bt):
+		fmt.Printf("[%6dms] SysEx     % X\n", timestampms, bt)
+
+	default:
+		fmt.Printf("[%6dms] Other     %v\n", timestampms, msg)
+	}
+}
+
 func main() {
 	defer midi.CloseDriver()
+
+	discoverKasa()
 
 	fmt.Println("Available MIDI input ports:")
 	fmt.Println(midi.GetInPorts())
@@ -24,30 +103,7 @@ func main() {
 
 	fmt.Printf("Listening on: %s\n\n", in)
 
-	stop, err := midi.ListenTo(in, func(msg midi.Message, timestampms int32) {
-		var ch, key, vel, cc, val uint8
-		var bt []byte
-
-		switch {
-		case msg.GetNoteStart(&ch, &key, &vel):
-			fmt.Printf("[%6dms] NoteOn    ch=%d  key=%3d  vel=%3d  (note %s)\n",
-				timestampms, ch, key, vel, midi.Note(key))
-		case msg.GetNoteEnd(&ch, &key):
-			fmt.Printf("[%6dms] NoteOff   ch=%d  key=%3d           (note %s)\n",
-				timestampms, ch, key, midi.Note(key))
-		case msg.GetControlChange(&ch, &cc, &val):
-			fmt.Printf("[%6dms] CC        ch=%d  cc=%3d   val=%3d\n",
-				timestampms, ch, cc, val)
-		case msg.GetAfterTouch(&ch, &val):
-			fmt.Printf("[%6dms] AfterTch  ch=%d  val=%3d\n",
-				timestampms, ch, val)
-		case msg.GetSysEx(&bt):
-			fmt.Printf("[%6dms] SysEx     % X\n", timestampms, bt)
-		default:
-			fmt.Printf("[%6dms] Other     %v\n", timestampms, msg)
-		}
-	}, midi.UseSysEx())
-
+	stop, err := midi.ListenTo(in, handleMIDI, midi.UseSysEx())
 	if err != nil {
 		fmt.Printf("error listening: %s\n", err)
 		os.Exit(1)
