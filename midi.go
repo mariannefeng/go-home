c41abd807dc26d78150a20eb7be14be7ff519252
Marianne Feng <feng.marianne@gmail.com>
2026-03-15 21:06:04 -0400

    extend lockout duration to 30 seconds

--- midi.go
+++ midi.go
@@ -81,7 +81,7 @@ var rainbowPalette = []uint8{5, 9, 13, 21, 33, 45, 53, 57}
 
 func runPadAnimation() {
 	const (
-		duration  = 15 * time.Second
+		duration  = 30 * time.Second
 		frameRate = 50 * time.Millisecond
 	)
 
